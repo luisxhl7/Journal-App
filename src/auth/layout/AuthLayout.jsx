d12ce3006285fb0b6f9b575c5c163c66230ed796
Luis carlos <luisxhl7@gmail.com>
2024-02-12 11:39:52 -0500

    adding layout of auth

---
+++ src/auth/layout/AuthLayout.jsx
@@ -0,0 +1,28 @@
+import PropTypes from "prop-types";
+import { Grid, Typography } from "@mui/material"
+
+export const AuthLayout = ({children, title}) => {
+    return (
+        <Grid container 
+          spacing={0} 
+          direction='column'
+          alignItems='center'
+          justifyContent='center'
+          sx={{minHeight: '100vh', backgroundColor:'primary.main', p: 4}}
+        >
+          <Grid item
+            className="box-shadow"
+            xs={3}
+            sx={{backgroundColor:'white', p: 4, borderRadius: 2}}
+          >
+            <Typography variant="h5" sx={{mb:1}}> {title} </Typography>
+            {children}
+          </Grid>
+        </Grid>
+      )
+}
+
+AuthLayout.propTypes = {
+    children: PropTypes.node.isRequired,
+    title: PropTypes.string.isRequired
+};
