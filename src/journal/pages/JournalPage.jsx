ac7d7ff74d51190ff8f5d50ea96b909e47732bf1
Luis carlos <luisxhl7@gmail.com>
2024-02-12 18:18:47 -0500

    adding button float of add note and galery

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,11 @@
 import { JournalLayout } from "../layout"
 import { NothigSelectedView } from "../components/NothigSelectedView"
 import { NoteView } from "../components/NoteView"
+import { IconButton } from "@mui/material"
+import { AddOutlined } from "@mui/icons-material"
 
 export const JournalPage = () => {
-  const haveNote = true
+  const haveNote = false
   return (
     <JournalLayout>
       {haveNote ?
@@ -11,6 +13,20 @@ export const JournalPage = () => {
         :
         <NothigSelectedView/>
       }
+      <IconButton
+        size="large"
+        sx={{
+          color: 'white', 
+          backgroundColor:'error.main',
+          ':hover': {backgroundColor: 'error.main', opacity: 0.9},
+          position: 'fixed',
+          right: 50,
+          bottom: 50,
+
+        }}
+      >
+        <AddOutlined sx={{fontSize: 30 }}/>
+      </IconButton>
     </JournalLayout>
   )
 }
